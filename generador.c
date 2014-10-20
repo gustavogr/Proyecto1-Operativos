089bd4e9795c3688c17b2698afe275f942c3aaaa
Guti <gustavogut1993@gmail.com>
2014-10-20 13:01:05 -0430

    Dos generadores y makefile

--- generador.c
+++ generador.c
@@ -1,42 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <errno.h>
 
 int main(int argc, char const *argv[])
 {
-	int cant = (int)strtol(argv[2], (char **)NULL, 10);
-	int i,num[cant],num1[cant];
-	FILE *fp;
-	srand(time(NULL));
-	for (i = 0; i < cant; ++i)
+	if (argc == 1)
 	{
-		num[i] = rand();
+		printf("No hay argumentos.\n");
+		return 1;
 	}
-
-
-	// char *mensaje = "Error en los argumentos. Use generador [nombre_archivo] [cantidad de numeros]."
+	int i,num[argc-2];
+	FILE *fp;
+	for (i = 0; i < argc - 2; ++i)
+	{
+		errno = 0;
+		num[i] = strtol(argv[i+2],NULL,10);
+		if (errno)
+		{
+			printf("Argumento %d invalido.\n", i+1);
+			return 1;
+		}
+	}
 	if ((fp = fopen(argv[1], "w")) == NULL)
 		perror("fopen:");
-	if (fwrite(&num[0], sizeof(int), cant, fp) == 0)
-		perror("fwrite:");
+	if (fwrite(&num[0], sizeof(int), argc-1, fp) == 0)
+		perror("fwrite");
 	fclose(fp);
-	
-	// Probando el archivo 
-
-	if ((fp = fopen(argv[1], "r")) == NULL) {
-		perror("**fopen:");
-		exit(0);
-	}
-	if (fread(&num1[0], sizeof(int), cant, fp) == 0) {
-		perror("fread:");
-		exit(0);
-	}
-	fclose(fp);
-
-	for (i = 0; i < cant; ++i)
-	{
-		printf("%d ",num1[i]);
-	}
-	printf("\n");
 	return 0;
 }
