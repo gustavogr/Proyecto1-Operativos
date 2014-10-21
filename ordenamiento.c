e0fd246c2756cafd577785b47f2a4749d0d49e54
Genessis Sanchez <genny@genny.ac.labf.usb.ve>
2014-10-21 12:15:56 -0430

    Ordenamiento listo

--- ordenamiento.c
+++ ordenamiento.c
@@ -4,16 +4,16 @@ int * merge(int arreglo1[], int arreglo2[], int tam1, int tam2)
 {
     int *auxiliar =(int *)malloc(sizeof(int)*(tam1+tam2));
     int i = 0, j = 0, k = 0;
-    while ((i <= (tam1 - 1)) && (j <= (tam2 - 1))) {
+    while ((i < tam1) && (j < tam2)) {
         if (arreglo1[i] <= arreglo2[j]) {
             auxiliar[k++] = arreglo1[i++];
         } else {
             auxiliar[k++] = arreglo2[j++];
         }
     }
-    while (i <= tam1 -1)
+    while (i < tam1)
         auxiliar[k++] = arreglo1[i++];  
-    while (j <= tam2 -1)
+    while (j < tam2)
         auxiliar[k++] = arreglo2[j++];
     return auxiliar;
 }
@@ -44,17 +44,61 @@ void quicksort(int arreglo[], int tam)
 {
     quicksort_rec(arreglo, 0, tam-1);
 }
-  
-/*int main(int argc, const char * argv[])
+
+int * merge_h(int arreglo[], int ini, int med, int fin)
+{
+    int *auxiliar = (int *)malloc(sizeof(int)*(fin-ini));
+    int i = ini, j = med, k = 0;
+    while ((i < med) && (j < fin)) {
+        if (arreglo[i] <= arreglo[j]) {
+            auxiliar[k++] = arreglo[i++];
+        } else {
+            auxiliar[k++] = arreglo[j++];
+        }
+    }
+    while (i < med)
+        auxiliar[k++] = arreglo[i++];  
+    while (j < fin)
+        auxiliar[k++] = arreglo[j++];
+    return auxiliar;
+}
+
+
+int main(int argc, const char * argv[])
 {
     int lista1[] = {1, 3, 5 , 7, 9};
     int lista2[] = {0, 2, 4 , 6, 8, 8, 8, 8, 10, 12};
+    int lista3[] = {2, 4, 6, -47, -1, 3};
+    int lista4[] = {5, 48, 39, 72 ,1435 ,13548};
     int t1 = sizeof(lista1)/sizeof(int);
     int t2 = sizeof(lista2)/sizeof(int);
+    int t3 = sizeof(lista3)/sizeof(int);
+    int t4 = sizeof(lista4)/sizeof(int);
+
     int *arreglo = merge(lista1, lista2, t1, t2);
+    int *arreglo1 = merge_h(lista3, 0, 3, 6);
+    quicksort_rec(lista4, 0, t4 -1);
+    //quicksort(lista4, t4);
+
     int g;
+    printf("Merge procesos:\n");  
     for (g = 0; g < t1+t2; g++){
         printf("%d ", arreglo[g]);
     }
     printf("\n");
-}*/
+    printf("Merge hilos:\n");  
+    for (g = 0; g < t3; g++){
+        printf("%d ", arreglo1[g]);
+    }
+    printf("\n");
+    printf("quicksort hilos (recursivo):\n");  
+    for (g = 0; g < t4; g++){
+        printf("%d ", lista4[g]);
+    }
+    printf("\n");
+    /*printf("quicksort procesos:\n");      
+    for (g = 0; g < t4; g++){
+        printf("%d ", lista4[g]);
+    }
+    printf("\n")*/;
+}
